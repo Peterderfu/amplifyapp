2b8e83f8da2835abebdb4da71aa80d0b5ced057e
Peter.Hsieh <peter.hsieh@ecloudvalley.com>
2024-01-25 17:04:39 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import './styles.css';
-// import { theme } from './theme';
 import studioTheme from './ui-components/studioTheme';
-import Example from './Example';
+import SettingsPage from './pages/SettingsPage';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -13,7 +12,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <ThemeProvider theme={studioTheme}>
-      <Example />
+      <SettingsPage />
     </ThemeProvider>
   </StrictMode>
 );
