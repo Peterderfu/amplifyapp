3716c1f83d3e88586ad688ff376d1c0da73a4d67
Peter.Hsieh <peter.hsieh@ecloudvalley.com>
2024-02-04 16:03:45 +0800

    test

--- src/pages/userPage.jsx
+++ src/pages/userPage.jsx
@@ -1,6 +1,6 @@
-import { Flex, Text, Divider, Button, SwitchField, Link } from '@aws-amplify/ui-react';
-// import { NavBarHeader, SideBar } from '../ui-components';
-import { SideBar } from '../ui-components';
+import { Flex, Loader } from '@aws-amplify/ui-react';
+import { NavBarHeader, SideBar } from '../ui-components';
+// import { SideBar } from '../ui-components';
 import DataFetcher from '../utils/DataFetcher';
 import { useState, useEffect } from 'react';
 import parse from 'html-react-parser';
@@ -16,27 +16,51 @@ import {
 const BasicExample = () => {
   let users = null;
   const [rows, setRows] = useState(null);
+  const [loading,setLoading] = useState(true);
   useEffect(() => {
     const resp = DataFetcher();
     resp.then(resp => {
       const user_json = JSON.parse(resp);
       users = user_json.data;
       setRows(parse(users.map(({ name, disabled, id }) => `<TableRow className=amplify-table__row><TableCell className=amplify-table__td>${name}</TableCell><TableCell className=amplify-table__td>${disabled ? "N" : "Y"}</TableCell><TableCell className=amplify-table__td>${id}</TableCell></TableRow>`).join('')));
+      setLoading(false)
     });
   }, []);
-  return (<Flex direction="column">
-    <Table caption="xxxxx" highlightOnHover={true} size="small">
-      <TableHead>
-        <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell>Enable</TableCell>
-          <TableCell>ID</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows}
-      </TableBody>
-    </Table>
-  </Flex>)
+  return (
+    <Flex
+      gap="0"
+      direction="column"
+      width="1440px"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+      overflow="hidden"
+      position="relative"
+      backgroundColor="rgba(255,255,255,1)"
+    >
+      <NavBarHeader />
+      <Flex
+        gap="0"
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        shrink="0"
+        alignSelf="stretch"
+        position="relative"
+      >
+        <SideBar />
+        <Table caption="Registered users" highlightOnHover={true} size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Enable</TableCell>
+              <TableCell>ID</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {loading?<Loader size="small" variation="linear" />:rows}
+          </TableBody>
+        </Table>
+      </Flex>
+    </Flex>)
 };
 export default BasicExample;
